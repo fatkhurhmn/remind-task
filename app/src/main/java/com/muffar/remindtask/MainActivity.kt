b47c00f163bac39773266e247450f666741e126f
fatkhu <fatkhurohman019@gmail.com>
2024-03-22 16:28:46 +0700

    setup UI theme

--- app/src/main/java/com/muffar/remindtask/MainActivity.kt
+++ app/src/main/java/com/muffar/remindtask/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.muffar.remindtask
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -15,11 +20,24 @@ import com.muffar.remindtask.ui.theme.RemindTaskTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                scrim = Color.TRANSPARENT,
+                darkScrim = Color.TRANSPARENT
+            ),
+            navigationBarStyle = SystemBarStyle.light(
+                scrim = Color.TRANSPARENT,
+                darkScrim = Color.TRANSPARENT
+            )
+        )
+
         setContent {
             RemindTaskTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier
+                        .systemBarsPadding()
+                        .navigationBarsPadding()
+                        .fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
--- app/src/main/java/com/muffar/remindtask/ui/theme/Theme.kt
+++ app/src/main/java/com/muffar/remindtask/ui/theme/Theme.kt
@@ -1,70 +1,114 @@
 package com.muffar.remindtask.ui.theme
 
 import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+import com.muffar.remindtask.ui.theme.color.MainColor
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = MainColor.Blue.primary,
+    onPrimary = MainColor.White,
+    primaryContainer = MainColor.Blue.light,
+    onPrimaryContainer = MainColor.White,
+    inversePrimary = MainColor.Blue.dark,
+
+    secondary = MainColor.Yellow.primary,
+    onSecondary = MainColor.White,
+    secondaryContainer = MainColor.Yellow.light,
+    onSecondaryContainer = MainColor.White,
+
+    tertiary = MainColor.Green.primary,
+    onTertiary = MainColor.White,
+    tertiaryContainer = MainColor.Green.light,
+    onTertiaryContainer = MainColor.White,
+
+    error = MainColor.Red.primary,
+    onError = MainColor.White,
+    errorContainer = MainColor.Red.light,
+    onErrorContainer = MainColor.White,
+
+    background = MainColor.Black,
+    onBackground = MainColor.White,
+    surface = MainColor.Black,
+    onSurface = MainColor.White,
+    surfaceVariant = MainColor.ExtraDarkGray,
+    onSurfaceVariant = MainColor.White,
+    surfaceTint = MainColor.White,
+    inverseSurface = MainColor.LightGray,
+    inverseOnSurface = MainColor.Black,
+
+    outline = MainColor.Gray,
+    outlineVariant = MainColor.LightGray,
+    scrim = MainColor.ExtraLightGray.copy(alpha = 0.8f)
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = MainColor.Blue.primary,
+    onPrimary = MainColor.White,
+    primaryContainer = MainColor.Blue.light,
+    onPrimaryContainer = MainColor.White,
+    inversePrimary = MainColor.Blue.dark,
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    secondary = MainColor.Yellow.primary,
+    onSecondary = MainColor.White,
+    secondaryContainer = MainColor.Yellow.light,
+    onSecondaryContainer = MainColor.White,
+
+    tertiary = MainColor.Green.primary,
+    onTertiary = MainColor.White,
+    tertiaryContainer = MainColor.Green.light,
+    onTertiaryContainer = MainColor.White,
+
+    error = MainColor.Red.primary,
+    onError = MainColor.White,
+    errorContainer = MainColor.Red.light,
+    onErrorContainer = MainColor.White,
+
+    background = MainColor.White,
+    onBackground = MainColor.Black,
+    surface = MainColor.White,
+    onSurface = MainColor.Black,
+    surfaceVariant = MainColor.ExtraLightGray,
+    onSurfaceVariant = MainColor.Black,
+    surfaceTint = MainColor.Black,
+    inverseSurface = MainColor.DarkGray,
+    inverseOnSurface = MainColor.White,
+
+    outline = MainColor.Gray,
+    outlineVariant = MainColor.DarkGray,
+    scrim = MainColor.ExtraDarkGray.copy(alpha = 0.8f)
 )
 
 @Composable
 fun RemindTaskTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit,
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            window.statusBarColor = Color.Transparent.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+    CompositionLocalProvider(LocalSpacing provides Spacing()) {
+        MaterialTheme(
+            colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme,
+            typography = Typography,
+            content = content,
+            shapes = Shapes
+        )
+    }
 }
